a7dad3ed0f049290d76e0a16b5dda36c106fae62
Myles Megyesi <myles.megyesi@gmail.com>
2011-10-11 04:11:57 -0500

    Commit full build, whoops

---
+++ static/sproutcore/ace/en/1.0/javascript.js
@@ -0,0 +1,451 @@
+/* >>>>>>>>>> BEGIN __sc_chance.js */
+if (typeof CHANCE_SLICES === 'undefined') var CHANCE_SLICES = [];CHANCE_SLICES = CHANCE_SLICES.concat(['__s39_0',
+'__s39_1',
+'__s39_2',
+'__s39_3',
+'__s39_4',
+'__s39_5',
+'__s39_6',
+'__s39_7',
+'__s39_8',
+'__s39_9',
+'__s39_10',
+'__s39_11',
+'__s40_12',
+'__s40_13',
+'__s40_14',
+'__s40_15',
+'__s40_16',
+'__s40_17',
+'__s40_18',
+'__s40_19',
+'__s40_20',
+'__s40_21',
+'__s40_22',
+'__s40_23',
+'__s40_24',
+'__s40_25',
+'__s40_26',
+'__s40_27',
+'__s40_28',
+'__s40_29',
+'__s40_30',
+'__s40_31',
+'__s40_32',
+'__s40_33',
+'__s40_34',
+'__s40_35',
+'__s40_36',
+'__s40_37',
+'__s40_38',
+'__s40_39',
+'__s41_40',
+'__s41_41',
+'__s41_42',
+'__s41_43',
+'__s41_44',
+'__s41_45',
+'__s41_46',
+'__s41_47',
+'__s41_48',
+'__s41_49',
+'__s41_50',
+'__s41_51',
+'__s41_52',
+'__s41_53',
+'__s41_54',
+'__s41_55',
+'__s41_56',
+'__s41_57',
+'__s41_58',
+'__s41_59',
+'__s42_60',
+'__s42_61',
+'__s42_62',
+'__s42_63',
+'__s42_64',
+'__s42_65',
+'__s42_66',
+'__s42_67',
+'__s42_68',
+'__s42_69',
+'__s42_70',
+'__s42_71',
+'__s44_72',
+'__s44_73',
+'__s44_74',
+'__s44_75',
+'__s44_76',
+'__s44_77',
+'__s44_78',
+'__s44_79',
+'__s44_80',
+'__s44_81',
+'__s44_82',
+'__s44_83',
+'__s44_84',
+'__s44_85',
+'__s44_86',
+'__s44_87',
+'__s44_88',
+'__s44_89',
+'__s44_90',
+'__s44_91',
+'__s44_92',
+'__s44_93',
+'__s44_94',
+'__s44_95',
+'__s44_96',
+'__s44_97',
+'__s44_98',
+'__s44_99',
+'__s45_100',
+'__s45_101',
+'__s45_102',
+'__s45_103',
+'__s45_104',
+'__s45_105',
+'__s45_106',
+'__s45_107',
+'__s45_108',
+'__s45_109',
+'__s45_110',
+'__s45_111',
+'__s45_112',
+'__s45_113',
+'__s45_114',
+'__s45_115',
+'__s45_116',
+'__s45_117',
+'__s45_118',
+'__s45_119',
+'__s47_120',
+'__s47_121',
+'__s47_122',
+'__s47_123',
+'__s47_124',
+'__s47_125',
+'__s48_126',
+'__s48_127',
+'__s48_128',
+'__s48_129',
+'__s48_130',
+'__s48_131',
+'__s49_132',
+'__s49_133',
+'__s49_134',
+'__s49_135',
+'__s49_136',
+'__s49_137',
+'__s53_138',
+'__s54_139',
+'__s54_140',
+'__s54_141',
+'__s54_142',
+'__s59_143',
+'__s59_144',
+'__s59_145',
+'__s59_146',
+'__s59_147',
+'__s59_148',
+'__s59_149',
+'__s59_150',
+'__s59_151',
+'__s59_152',
+'__s59_153',
+'__s59_154',
+'__s59_155',
+'__s59_156',
+'__s61_157',
+'__s61_158',
+'__s61_159',
+'__s61_160',
+'__s61_161',
+'__s61_162',
+'__s61_163',
+'__s61_164',
+'__s61_165',
+'__s62_166',
+'__s62_167',
+'__s62_168',
+'__s62_169',
+'__s62_170',
+'__s62_171',
+'__s62_172',
+'__s62_173',
+'__s62_174',
+'__s62_175',
+'__s62_176',
+'__s62_177',
+'__s62_178',
+'__s63_179',
+'__s63_180',
+'__s63_181',
+'__s63_182',
+'__s63_183',
+'__s63_184',
+'__s63_185',
+'__s63_186',
+'__s63_187',
+'__s63_188',
+'__s63_189',
+'__s63_190',
+'__s63_191',
+'__s63_192',
+'__s63_193',
+'__s63_194',
+'__s63_195',
+'__s63_196',
+'__s63_197',
+'__s63_198',
+'__s63_199',
+'__s63_200',
+'__s63_201',
+'__s63_202',
+'__s63_203',
+'__s63_204',
+'__s64_205',
+'__s64_206',
+'__s64_207',
+'__s64_208',
+'__s64_209',
+'__s64_210',
+'__s65_211',
+'__s65_212',
+'__s65_213',
+'__s65_214',
+'__s65_215',
+'__s65_216',
+'__s66_217',
+'__s66_218',
+'__s66_219',
+'__s66_220',
+'__s66_221',
+'__s66_222',
+'__s66_223',
+'__s66_224',
+'__s66_225',
+'__s68_226',
+'__s68_227',
+'__s68_228',
+'__s68_229',
+'__s68_230',
+'__s68_231',
+'__s68_232',
+'__s68_233',
+'__s68_234',
+'__s70_235',
+'__s70_236',
+'__s70_237',
+'__s70_238',
+'__s70_239',
+'__s70_240',
+'__s70_241',
+'__s70_242',
+'__s70_243',
+'__s70_244',
+'__s70_245',
+'__s70_246',
+'__s70_247',
+'__s70_248',
+'__s70_249',
+'__s70_250',
+'__s70_251',
+'__s70_252',
+'__s70_253',
+'__s70_254',
+'__s71_255',
+'__s71_256',
+'__s71_257',
+'__s71_258',
+'__s71_259',
+'__s71_260',
+'__s71_261',
+'__s71_262',
+'__s71_263',
+'__s71_264',
+'__s71_265',
+'__s71_266',
+'__s71_267',
+'__s71_268',
+'__s71_269',
+'__s71_270',
+'__s71_271',
+'__s71_272',
+'__s71_273',
+'__s71_274',
+'__s72_275',
+'__s72_276',
+'__s72_277',
+'__s72_278',
+'__s72_279',
+'__s72_280',
+'__s72_281',
+'__s72_282',
+'__s72_283',
+'__s72_284',
+'__s72_285',
+'__s72_286',
+'__s72_287',
+'__s72_288',
+'__s72_289',
+'__s72_290',
+'__s72_291',
+'__s72_292',
+'__s72_293',
+'__s72_294',
+'__s73_295',
+'__s73_296',
+'__s73_297',
+'__s73_298',
+'__s73_299',
+'__s73_300',
+'__s73_301',
+'__s73_302',
+'__s73_303',
+'__s73_304',
+'__s73_305',
+'__s73_306',
+'__s73_307',
+'__s73_308',
+'__s73_309',
+'__s73_310',
+'__s73_311',
+'__s73_312',
+'__s73_313',
+'__s73_314',
+'__s75_315',
+'__s75_316',
+'__s75_317',
+'__s75_318',
+'__s75_319',
+'__s76_320',
+'__s76_321',
+'__s77_322',
+'__s77_323',
+'__s77_324',
+'__s77_325',
+'__s77_326',
+'__s81_327']);
+
+/* >>>>>>>>>> BEGIN source/theme.js */
+
+/**
+ * @class
+ * SproutCore's Ace theme.
+ */
+SC.AceTheme = SC.BaseTheme.create({
+  name: "ace",
+  description: "A SproutCore built-in theme by Alex Iskander and contributors. Only supports browsers that implement CSS3."
+});
+
+// register the theme with SproutCore
+SC.Theme.addTheme(SC.AceTheme);
+
+// SC.ButtonView variants
+SC.AceTheme.PointLeft = SC.AceTheme.subtheme("point-left", "point-left");
+SC.AceTheme.PointRight = SC.AceTheme.subtheme("point-right", "point-right");
+SC.AceTheme.Capsule = SC.AceTheme.subtheme("capsule", "capsule");
+
+// Dark Variant
+/**
+ * @class SC.AceTheme.Dark
+ * SproutCore's Ace theme's Dark Side. Used in popovers or wherever you
+ * choose (use it by making the view or one of its parents have a
+ * themeName of 'dark').
+ */
+SC.AceTheme.Dark = SC.AceTheme.subtheme("dark");
+
+// for backwards-compatibility with apps that do not set their
+// own default theme:
+SC.defaultTheme = 'ace';
+
+/* >>>>>>>>>> BEGIN source/resources/button/dark/button.js */
+sc_require("theme");
+
+SC.AceTheme.Dark.PointLeft = SC.AceTheme.Dark.subtheme("point-left", "point-left");
+SC.AceTheme.Dark.PointRight = SC.AceTheme.Dark.subtheme("point-right", "point-right");
+SC.AceTheme.Dark.PointRight = SC.AceTheme.Dark.subtheme("capsule", "capsule");
+
+/* >>>>>>>>>> BEGIN source/resources/collection/source-list/source-list.js */
+SC.AceTheme.SourceList = SC.AceTheme.subtheme('source-list');
+
+// later we may define/override some constants here
+/* >>>>>>>>>> BEGIN source/resources/picker/ace/picker.js */
+
+/* >>>>>>>>>> BEGIN source/resources/picker/popover/popover.js */
+sc_require("theme");
+
+// it is derived from dark, but will be available both under Ace and not.
+SC.AceTheme.Popover = SC.AceTheme.Dark.subtheme("popover");
+SC.AceTheme.addTheme(SC.AceTheme.Popover);
+
+// there is a solid variety
+SC.AceTheme.SolidPopover = SC.AceTheme.Popover.subtheme('solid');
+
+// and a shortcut to the solid variety.
+SC.AceTheme.addTheme(SC.AceTheme.SolidPopover.create({ name: 'solid-popover' }));
+
+
+/* >>>>>>>>>> BEGIN source/resources/picker/popover/picker.js */
+// ==========================================================================
+// Project:   SproutCore - JavaScript Application Framework
+// Copyright: ©2006-2011 Strobe Inc. and contributors.
+//            Portions ©2008-2011 Apple Inc. All rights reserved.
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
+sc_require("resources/picker/popover/popover");
+
+SC.AceTheme.Popover.pickerRenderDelegate = SC.RenderDelegate.create({
+  className: 'picker',
+  
+  render: function(dataSource, context) {
+    var preferType = dataSource.get('preferType');
+    var pointerPosition = dataSource.get('pointerPos');
+    var pointerPositionY = dataSource.get('pointerPosY');
+
+    if (preferType == SC.PICKER_POINTER || preferType == SC.PICKER_MENU_POINTER) {
+      context.addClass(pointerPosition);
+    }
+  },
+  
+  update: function(dataSource, $) {
+    var preferType = dataSource.get('preferType');
+    var pointerPosition = dataSource.get('pointerPos');
+    var pointerPositionY = dataSource.get('pointerPosY');
+    
+    if (preferType == SC.PICKER_POINTER || preferType == SC.PICKER_MENU_POINTER) {
+      $.addClass(pointerPosition);
+    }
+    
+  }
+});
+/* >>>>>>>>>> BEGIN source/resources/picker/popover/workspace.js */
+// ==========================================================================
+// Project:   SproutCore - JavaScript Application Framework
+// Copyright: ©2006-2011 Strobe Inc. and contributors.
+//            Portions ©2008-2011 Apple Inc. All rights reserved.
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
+
+SC.AceTheme.Popover.workspaceRenderDelegate = SC.RenderDelegate.create({
+  className: 'workspace',
+  render: function(dataSource, context) {
+    context.setClass({
+      'top-toolbar': dataSource.get('hasTopToolbar'),
+      'bottom-toolbar': dataSource.get('hasBottomToolbar')
+    });
+    
+    context = context.begin('div').addClass('popover-background');
+    this.includeSlices(dataSource, context, SC.NINE_SLICE);
+    context.push("<div class = 'sc-pointer'></div>");
+    context = context.end();
+  },
+
+  update: function(dataSource, jquery) {
+    jquery.setClass({
+      'top-toolbar': dataSource.get('hasTopToolbar'),
+      'bottom-toolbar': dataSource.get('hasBottomToolbar')
+    });
+  }
+});
